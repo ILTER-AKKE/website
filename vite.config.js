0ddf50f8a3b4e0be1e3b01021d69255f4dce7374
Berk ÇAKMAK <berkcakmak5@gmail.com>
2025-10-31 05:22:14 +0300

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sl-glove-websit/',
+  base: '/website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
